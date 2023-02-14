3003cdfa80dbaaa0a6bae74a09b430e8381e0608
Bedalbekov Bekulan <bedalbeka481@gmail.com>
2023-02-14 16:51:19 +0600

    Initial commit

---
+++ src/LimitException.java
@@ -0,0 +1,16 @@
+public class LimitException extends Exception{
+
+    private Double remainingAmount;
+
+    public LimitException(String message,Double remainingAmount) {
+        super(message);
+        this.remainingAmount = remainingAmount;
+
+    }
+
+    public Double getRemainingAmount() {
+        return remainingAmount;
+    }
+}
+
+
